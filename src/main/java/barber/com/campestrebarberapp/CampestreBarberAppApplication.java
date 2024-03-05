d8b34094c190ae2dc3c9018e49ba836b0befd37a
csandanielo <csandanielo@gmail.com>
2024-03-04 22:05:09 -0300

    Commit inicial do projeto

---
+++ src/main/java/barber/com/campestrebarberapp/CampestreBarberAppApplication.java
@@ -0,0 +1,13 @@
+package barber.com.campestrebarberapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampestreBarberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampestreBarberAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/barber/com/campestrebarberapp/CampestreBarberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package barber.com.campestrebarberapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampestreBarberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
